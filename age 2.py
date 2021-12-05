3f7411150d6438c7a964ac352f57af93c68733e1
SatishGundubilli <95543206+SatishGundubilli@users.noreply.github.com>
2021-12-05 22:29:29 +0530

    Add files via upload

---
+++ age 2.py
@@ -0,0 +1,10 @@
+age = int(input("enter a your age"))
+if age<12:
+    print("children")
+elif 13<=age<=19:
+        print("teenz")
+elif 20<=age<=35:
+            print("adults")
+            
+else:
+    print("old")
