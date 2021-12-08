ab3459ffb0c9d24148e621bfea83dbe594dea6a8
SatishGundubilli <95543206+SatishGundubilli@users.noreply.github.com>
2021-12-08 10:43:32 +0530

    Add files via upload

---
+++ userdefunction 1.py
@@ -0,0 +1,14 @@
+a,b,c,d= map(str,input("enter the names:").split())
+def satish(a,b,c,d):
+    if a>b and a>b and a>c and a>d:
+            print(a, "is large num")
+    elif b>a and b>c and b>d:
+            print(b, "nnnn")
+    elif c>a and c>b and c>d:
+            print(c, "nnn")
+    else:
+        print(d)
+    
+        
+
+satish(a,b,c,d)
